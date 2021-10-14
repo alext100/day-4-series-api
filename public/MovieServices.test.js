2cfd6da21cdba3a302406a35a976c8758ba02049
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-10-14 20:47:31 +0200

    Test for a deleteSerie function added

--- public/MovieServices.test.js
+++ public/MovieServices.test.js
@@ -1,4 +1,5 @@
 import MoviesServices from "./MovieServices.js";
+import "isomorphic-fetch";
 
 describe("Given a getSeries function", () => {
   describe("When it recives response from API with url 'http://localhost:3001/series/'", () => {
@@ -9,6 +10,20 @@ describe("Given a getSeries function", () => {
       const expectResponse = getSeries.getSeries();
 
       return expect(typeof expected).toEqual(typeof expectResponse);
+    });
+  });
+});
+
+describe("Given a deleteSerie function", () => {
+  describe("When it recives id propertie of an object", () => {
+    test("Then it should return the empty object", () => {
+      const expected = true;
+      const id = 2;
+
+      const getSeries = new MoviesServices();
+      const result = getSeries.deleteSerie(id);
+
+      return expect(result).resolves.toEqual(expected);
     });
   });
 });
